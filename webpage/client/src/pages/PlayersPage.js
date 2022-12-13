679d0d032f1b671000dbe843a8dad605f745174f
friedcarrot <115592620+yingzhe1234@users.noreply.github.com>
2022-12-13 05:28:52 -0500

    Change to Players
    I made some changes to front end "PlayersPage" file, and also modified its backend "routes" part

--- webpage/client/src/pages/PlayersPage.js
+++ webpage/client/src/pages/PlayersPage.js
@@ -27,6 +27,7 @@ const playerColumns = [
         dataIndex: 'Name',
         key: 'Name',
         sorter: (a, b) => a.Name.localeCompare(b.Name),
+        render: (text, row) => <a href={`/players?id=${row.PlayerId}`}>{text}</a>
     },
     {
         title: 'Team',
@@ -39,10 +40,24 @@ const playerColumns = [
         dataIndex: 'Season',
         key: 'Season',
         sorter: (a, b) => a.Season - b.Season
-
     },
-    // TASK 19: copy over your answers for tasks 7 - 9 to add columns for potential, club, and value
-    // TASK 7: add a column for Potential, with the ability to (numerically) sort ,
+    {
+        title: 'pts',
+        dataIndex: 'pts',
+        key: 'pts',
+        sorter: (a, b) => a.pts - b.pts
+    },
+    {
+        title: 'AST',
+        dataIndex: 'AST',
+        key: 'AST',
+        sorter: (a, b) => a.AST - b.AST
+    },{
+        title: 'REB',
+        dataIndex: 'REB',
+        key: 'REB',
+        sorter: (a, b) => a.REB - b.REB
+    },
 ];
 
 
@@ -56,10 +71,9 @@ class PlayersPage extends React.Component {
             ptsQuery: '',
             atsQuery: '',
             rbsQuery: '',
-            selectedPlayerId: window.location.search ? window.location.search.substring(1).split('=')[1] : 229594,
+            selectedPlayerId: window.location.search ? window.location.search.substring(1).split('=')[1] : 201166,
             selectedPlayerDetails: null,
             playersResults: []
-
         }
 
         this.updateSearchResults = this.updateSearchResults.bind(this)
@@ -104,8 +118,8 @@ class PlayersPage extends React.Component {
 
         // TASK 25: call getPlayer with the appropriate parameter and set update the correct state variable. 
         // See the usage of getMatch in the componentDidMount method of MatchesPage for a hint! 
-        getPlayer(this.state.selectedPlayerName).then(res => {
-            this.setState({ selectedPlayerDetails: res.results[0] })
+        getPlayer(this.state.selectedPlayerId).then(res => {
+            this.setState({ selectedPlayerDetails: res.results[0] })        
         })
     }
 
@@ -136,7 +150,7 @@ class PlayersPage extends React.Component {
 
                 </Form>
                 <Divider />
-                <Table onRow={(record, rowIndex) => {
+                {/* <Table onRow={(record, rowIndex) => {
                     return {
                         onClick: event => {this.goToPlayer(record.PlayerName)}, // clicking a row takes the user to a detailed view of the match in the /matches page using the MatchId parameter  
                     };
@@ -147,10 +161,135 @@ class PlayersPage extends React.Component {
                             <Column title="pts" dataIndex="pts" key="pts" sorter= {(a, b) => a.pts > b.pts }/>
                             <Column title="pts" dataIndex="ats" key="ats" sorter= {(a, b) => a.ats > b.ats }/>
                             <Column title="rbs" dataIndex="rbs" key="rbs" sorter= {(a, b) => a.rbs > b.rbs }/>
-                </Table>
+                </Table> */}
+                <Table dataSource={this.state.playersResults} columns={playerColumns} pagination={{ pageSizeOptions:[5, 10], defaultPageSize: 5, showQuickJumper:true }} style={{ width: '70vw', margin: '0 auto', marginTop: '2vh' }}/>
                 <Divider />
 
                 {this.state.selectedPlayerDetails ? <div style={{ width: '70vw', margin: '0 auto', marginTop: '2vh' }}>
+                    <Card>
+                    
+                        <CardBody>
+                        <Row gutter='30' align='middle' justify='center'>
+                            <Col flex={2} style={{ textAlign: 'left' }}>
+                            <h3>{this.state.selectedPlayerDetails.Name}</h3>
+
+                            </Col>
+
+                            <Col flex={2} style={{ textAlign: 'right' }}>
+                            <img src={this.state.selectedPlayerDetails.Photo} referrerpolicy="no-referrer" alt={null} style={{height:'15vh'}}/>
+
+                            </Col>
+                        </Row>
+                            <Row gutter='30' align='middle' justify='left'>
+                                <Col>
+                                <h5>{this.state.selectedPlayerDetails.Club}</h5>
+                                </Col>
+                                <Col>
+                                <h5>{this.state.selectedPlayerDetails.JerseyNumber}</h5>
+                                </Col>
+                                <Col>
+                                <h5>{this.state.selectedPlayerDetails.BestPosition}</h5>
+                                </Col>
+                            </Row>
+                            <br>
+                            </br>
+                            <Row gutter='30' align='middle' justify='left'>
+                                <Col>
+                                Age: {this.state.selectedPlayerDetails.Age}
+                                </Col>
+                                {/* TASK 28: add two more columns here for Height and Weight, with the appropriate labels as above */}
+                                <Col>
+                                Height: {this.state.selectedPlayerDetails.Height}
+                                </Col>
+                                <Col>
+                                Weight: {this.state.selectedPlayerDetails.Weight}
+                                </Col>
+
+                                <Col flex={2} style={{ textAlign: 'right' }}>
+                                {this.state.selectedPlayerDetails.Nationality}
+                                    <img src={this.state.selectedPlayerDetails.Flag} referrerpolicy="no-referrer" alt={null} style={{height:'3vh', marginLeft: '1vw'}}/>
+                                </Col>
+
+                            </Row>
+                            <Row gutter='30' align='middle' justify='left'>
+                                <Col>
+                                Value: {this.state.selectedPlayerDetails.Value}
+                                </Col>
+                                <Col>
+                                Release Clause: {this.state.selectedPlayerDetails.ReleaseClause}
+                                </Col>
+                                {/* TASK 29: Create 2 additional columns for the attributes 'Wage' and 'Contract Valid Until' (use spaces between the words when labelling!) */}
+                                <Col>
+                                Wage: {this.state.selectedPlayerDetails.Wage}
+                                </Col>
+                                <Col>
+                                Contract Valid Until: {this.state.selectedPlayerDetails.ContractValidUntil}
+                                </Col>
+                            </Row>
+                        </CardBody>
+
+                    </Card>
+
+                    <Card style={{marginTop: '2vh'}}>
+                        <CardBody>
+                            <Row gutter='30' align='middle' justify='center'>
+                            <Col flex={2} style={{ textAlign: 'left' }}>
+                            <h6>Skill</h6>
+                            <Rate disabled defaultValue={this.state.selectedPlayerDetails.Skill} />
+                            <h6>Reputation</h6>
+                            {/* TASK 30: create a star rating component for 'InternationalReputation'. Make sure you use the 'disabled' option as above to ensure it is read-only*/}
+                            <Rate disabled defaultValue={this.state.selectedPlayerDetails.InternationalReputation} />
+
+                            <Divider/>
+                            <h6>Best Rating</h6>
+                                <Progress style={{ width: '20vw'}} value={this.state.selectedPlayerDetails.BestOverallRating} >{this.state.selectedPlayerDetails.BestOverallRating}</Progress>
+                                {/* TASK 31: create the headings and progress bars for 'Potential' and 'Rating'. Use the same style as the one above for 'Best Rating'.*/}
+                            <h6>Potential</h6>
+                                <Progress style={{ width: '20vw'}} value={this.state.selectedPlayerDetails.Potential} >{this.state.selectedPlayerDetails.Potential}</Progress>
+                            <h6>Rating</h6>
+                                <Progress style={{ width: '20vw'}} value={this.state.selectedPlayerDetails.Rating} >{this.state.selectedPlayerDetails.Rating}</Progress>
+                                </Col >
+                                <Col  push={2} flex={2}>
+                                {/*TASK 32: In case the player is a GK, show a radar chart (replacing 'null' below) with the labels: Agility, Ball Control, Passing, Positioning, Stamina, Strength */}
+
+                                    {this.state.selectedPlayerDetails.BestPosition === 'GK'?
+                                        <RadarChart
+                                            data={[this.state.selectedPlayerDetails]}
+                                            tickFormat={t => wideFormat(t)}
+                                            startingAngle={0}
+                                            domains={[
+                                                { name: 'Penalties', domain: [0, 100], getValue: d => d.GKPenalties },
+                                                { name: 'Diving', domain: [0, 100], getValue: d => d.GKDiving },
+                                                { name: 'Handling', domain: [0, 100], getValue: d => d.GKHandling },
+                                                { name: 'Kicking', domain: [0, 100], getValue: d => d.GKKicking },
+                                                { name: 'Positioning', domain: [0, 100], getValue: d => d.GKPositioning },
+                                                { name: 'Reflexes', domain: [0, 100], getValue: d => d.GKReflexes }
+                                            ]}
+                                            width={450}
+                                            height={400}
+                                        />
+                                        :
+                                        <RadarChart
+                                            data={[this.state.selectedPlayerDetails]}
+                                            tickFormat={t => wideFormat(t)}
+                                            startingAngle={0}
+                                            domains={[
+                                                { name: 'Agility', domain: [0, 100], getValue: d => d.NAdjustedAgility },
+                                                { name: 'Ball Control', domain: [0, 100], getValue: d => d.NBallControl },
+                                                { name: 'Passing', domain: [0, 100], getValue: d => d.NPassing },
+                                                { name: 'Positioning', domain: [0, 100], getValue: d => d.NPositioning },
+                                                { name: 'Stamina', domain: [0, 100], getValue: d => d.NStamina },
+                                                { name: 'Strength', domain: [0, 100], getValue: d => d.NStrength }
+                                            ]}
+                                            width={450}
+                                            height={400}
+                                    
+                                        />}
+                                
+                                </Col>
+                            </Row>
+                        </CardBody>
+                    </Card>
 
                 </div> : null}
 
